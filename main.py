7ea1a49404cf1491d3ced6e0cf53e42993d939d0
Ethan Winters <ebwinters@comcast.net>
2022-03-21 23:01:35 +0000

    fix bug

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from table_storage import TableStorage
 
 tableStorage = TableStorage("homesecflags")
 motionState = False
-lastCheckedTime = datetime.datetime.now
+lastCheckedTime = datetime.datetime.now()
 shouldRunMotionDetection = True
 while True:
-    if lastCheckedTime < datetime.datetime.now:
+    if lastCheckedTime < datetime.datetime.now():
         shouldRunMotionDetection = tableStorage.GetShouldMonitorFlag()
-        lastCheckedTime = datetime.datetime.now
+        lastCheckedTime = datetime.datetime.now()
     motionState = picam.motion()
     print (motionState)
     if (motionState):
