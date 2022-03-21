c5b9e8fc38b8254a6d37972053b4219fb8c24f00
Ethan Winters <ebwinters@comcast.net>
2022-03-21 23:40:37 +0000

    stuffs

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ lastCheckedTime = datetime.datetime.now()
 shouldRunMotionDetection = True
 while True:
     currentDate = datetime.datetime.now()
-    if currentDate.second == 59 or currentDate.second == 60:
+    if currentDate.second >= 58 and currentDate.second <= 60:
         if lastCheckedTime < currentDate:
             shouldRunMotionDetection = tableStorage.GetShouldMonitorFlag()
+            sleep(2)
             lastCheckedTime = currentDate
     if (shouldRunMotionDetection):
         motionState = picam.motion()
