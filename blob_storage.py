d340344dd987d32acf785bea87e862a5a31723bd
ebwinters@comcast.net <ebwinters@comcast.net>
2022-03-25 07:52:51 -0700

    add read code

--- blob_storage.py
+++ blob_storage.py
@@ -5,8 +5,14 @@ from util import readAppSetting
 
 blob_service_client = BlobServiceClient.from_connection_string(readAppSetting("storageConnectionString"))
 blob_client = blob_service_client.get_blob_client(container="home-security-status", blob="status.json")
-print("\nUploading to Azure Storage as blob")
-data = {}
-data['status'] = 'on'
-json_data = json.dumps(data)
-blob_client.upload_blob(json_data)
+
+with open("./BlockDestination.txt", "wb") as my_blob:
+    blob_data = blob_client.download_blob()
+    blob_data.readinto(my_blob)
+    print(blob_data)
+
+# print("\nUploading to Azure Storage as blob")
+# data = {}
+# data['status'] = 'on'
+# json_data = json.dumps(data)
+# blob_client.upload_blob(json_data)
